560e3eda7eaee66cfe6980ac3bc3b9841ce84c90
unknown <you@example.com>
2024-06-10 02:44:06 +0800

    add rgba_to_i420

--- api/video/yuv_helper.cc
+++ api/video/yuv_helper.cc
@@ -413,4 +413,19 @@ int ARGBToNV12(const uint8_t* src_argb,
                             dst_uv, dst_stride_uv, width, height);
 }
 
+int RGBAToI420(const uint8_t* src_argb,
+               int src_stride_argb,
+               uint8_t* dst_y,
+               int dst_stride_y,
+               uint8_t* dst_u,
+               int dst_stride_u,
+               uint8_t* dst_v,
+               int dst_stride_v,
+               int width,
+               int height) {
+  return libyuv::RGBAToI420(src_argb, src_stride_argb, dst_y, dst_stride_y,
+                            dst_u, dst_stride_u, dst_v, dst_stride_v, width,
+                            height);
+}
+
 }  // namespace webrtc
--- api/video/yuv_helper.h
+++ api/video/yuv_helper.h
@@ -313,4 +313,14 @@ RTC_EXPORT int ARGBToNV12(const uint8_t* src_argb,
                           int width,
                           int height);
 
+RTC_EXPORT int RGBAToI420(const uint8_t* src_argb,
+                          int src_stride_argb,
+                          uint8_t* dst_y,
+                          int dst_stride_y,
+                          uint8_t* dst_u,
+                          int dst_stride_u,
+                          uint8_t* dst_v,
+                          int dst_stride_v,
+                          int width,
+                          int height);
 }  // namespace webrtc
